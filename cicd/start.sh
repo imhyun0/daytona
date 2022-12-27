1cf1d323b532fac130c222a814d7511928ebc9f1
hyeonyeong.im <hyeonyeong.im@lge.com>
2022-12-27 10:55:21 +0900

    test 11

--- cicd/start.sh
+++ cicd/start.sh
@@ -8,8 +8,9 @@ SORC_WAR_FILE=$(ls /home/ec2-user/deploy/*.war)
 WAR_FILE=simpleweb1.war
 
 echo "run application_start!!!"
-chown -R ${WAS_ADM_USER}:${WAS_ADM_USER} ${WORKING_DIR}
-su - ${WAS_ADM_USER} -c "cp ${WORKING_DIR}/${SORC_WAR_FILE} ${WAS_DIR}/${WAR_FILE}"
+cp ${WORKING_DIR}/${SORC_WAR_FILE} ${WAS_DIR}/${WAR_FILE}
+#chown -R ${WAS_ADM_USER}:${WAS_ADM_USER} ${WORKING_DIR}
+#su - ${WAS_ADM_USER} -c "cp ${WORKING_DIR}/${SORC_WAR_FILE} ${WAS_DIR}/${WAR_FILE}"
 
 # tomcat_pid=$(ps -ef | grep tomcat | grep java | awk '{print $2}')
 # if [ -n "${tomcat_pid}" ]; then
